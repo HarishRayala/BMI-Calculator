1a5a7cbefccdf21c2a324bd78b1bf569ba97ab4f
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-11-12 17:50:46 +0530

    get brand

--- myntra-backend/controller/Men.controller.js
+++ myntra-backend/controller/Men.controller.js
@@ -4,6 +4,10 @@ const getMenData=async(req,res)=>{
     const menData=await MensModel.find();
     res.send(menData)
 }
+const getBrand=async(req,res)=>{
+    const brandData=await MensModel.find({brand:req.params.brand});
+    res.send(brandData)
+}
 
 const postMensData=async(req,res)=>{
     const {Idno,categories,title,price,gender,sizes,description,brand,color,discount,off_price,images,rating}=req.body
@@ -19,6 +23,7 @@ const postMensData=async(req,res)=>{
 
 const MensDataController={
     getMenData,
+    getBrand,
     postMensData
 }
 
